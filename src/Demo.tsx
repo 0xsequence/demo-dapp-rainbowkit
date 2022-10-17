52f26c07de3650d109afff165ba73ce59c822463
sagostinelli <batonenbois@gmail.com>
2022-10-17 15:26:39 -0400

    added Console

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -4,7 +4,7 @@ import logoUrl from './images/logo.svg'
 
 import { ethers } from 'ethers'
 import { sequence } from '0xsequence'
-import { useConnectModal, ConnectButton } from '@rainbow-me/rainbowkit'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useSigner, useProvider, useAccount } from 'wagmi'
 
 
@@ -14,7 +14,7 @@ import { configureLogger } from '@0xsequence/utils'
 import { Group } from './components/Group'
 import { styled, typography } from './style'
 import { Button } from './components/Button'
-
+import { Console } from './components/Console'
 
 configureLogger({ logLevel: 'DEBUG' })
 
@@ -23,94 +23,195 @@ const App = () => {
   const provider = useProvider()
   const { data: signer } = useSigner()
 
+  const [consoleMsg, setConsoleMsg] = useState<null|string>(null)
+  const [consoleLoading, setConsoleLoading] = useState<boolean>(false)
+
+  const appendConsoleLine = (message: string) => {
+    return (setConsoleMsg((prevState => {
+      return `${prevState}\n\n${message}`
+    })))
+  }
+  
+  const resetConsole = () => {
+    setConsoleMsg(null)
+    setConsoleLoading(true)
+  }
+
+  const addNewConsoleLine = (message: string) => {
+    setConsoleMsg((() => {
+      return (message)
+    }))
+  }
+
+  const consoleWelcomeMessage = () => {
+    setConsoleLoading(false)
+    setConsoleMsg('Status: Wallet not connected. Please connect wallet to use Methods')
+  }
+
+  const consoleErrorMesssage = () => {
+    setConsoleLoading(false)
+    setConsoleMsg('An error occurred')
+  }
+
+  useEffect(() => {
+    if (isConnected) {
+      setConsoleMsg('Wallet connected!')
+    } else {
+      consoleWelcomeMessage()
+    }
+  }, [isConnected])
+
   const getChainID = async () => {
-    console.log('signer.getChainId()', await signer.getChainId())
+    try {
+      resetConsole()
+      const chainId = await signer.getChainId()
+      console.log('signer.getChainId()', chainId)
+      addNewConsoleLine(`signer.getChainId(): ${chainId}`)
+      setConsoleLoading(false)
+    } catch(e) {
+      console.error(e)
+      consoleErrorMesssage()
+    }
   }
 
   const getBalance = async () => {
-    const account = await signer.getAddress()
-    const balanceChk1 = await provider!.getBalance(account)
-    console.log('balance check 1', balanceChk1.toString())
-
-    const balanceChk2 = await signer.getBalance()
-    console.log('balance check 2', balanceChk2.toString())
+    try {
+      resetConsole()
+      const account = await signer.getAddress()
+      const balanceChk1 = await provider!.getBalance(account)
+      console.log('balance check 1', balanceChk1.toString())
+      addNewConsoleLine(`balance check 1: ${balanceChk1.toString()}`)
+  
+      const balanceChk2 = await signer.getBalance()
+      console.log('balance check 2', balanceChk2.toString())
+      appendConsoleLine(`balance check 2: ${balanceChk2.toString()}`)
+      setConsoleLoading(false) 
+    } catch(e) {
+      console.error(e)
+      consoleErrorMesssage()
+    }
   }
 
+
   const getNetworks = async () => {
-    console.log('networks:', await provider!.getNetwork())
+    try {
+      resetConsole()
+      const network = await provider!.getNetwork() 
+      console.log('networks:', network)
+  
+      addNewConsoleLine(`networks: ${JSON.stringify(network)}`)
+      setConsoleLoading(false) 
+    } catch(e) {
+      console.error(e)
+      consoleErrorMesssage()
+    }
   }
 
   const signMessage = async () => {
-    const message = `Two roads diverged in a yellow wood,
-Robert Frost poet
-
-And sorry I could not travel both
-And be one traveler, long I stood
-And looked down one as far as I could
-To where it bent in the undergrowth;
-
-Then took the other, as just as fair,
-And having perhaps the better claim,
-Because it was grassy and wanted wear;
-Though as for that the passing there
-Had worn them really about the same,
-
-And both that morning equally lay
-In leaves no step had trodden black.
-Oh, I kept the first for another day!
-Yet knowing how way leads on to way,
-I doubted if I should ever come back.
-
-I shall be telling this with a sigh
-Somewhere ages and ages hence:
-Two roads diverged in a wood, and I—
-I took the one less traveled by,
-And that has made all the difference.`
-
-    // sign
-    const sig = await signer.signMessage(message)
-    console.log('signature:', sig)
-
-    const isValid = await sequence.utils.isValidMessageSignature(await signer.getAddress(), message, sig, provider as ethers.providers.Web3Provider)
-    console.log('isValid?', isValid)
+    try {
+      resetConsole()  
+      const message = `Two roads diverged in a yellow wood,
+  Robert Frost poet
+  
+  And sorry I could not travel both
+  And be one traveler, long I stood
+  And looked down one as far as I could
+  To where it bent in the undergrowth;
+  
+  Then took the other, as just as fair,
+  And having perhaps the better claim,
+  Because it was grassy and wanted wear;
+  Though as for that the passing there
+  Had worn them really about the same,
+  
+  And both that morning equally lay
+  In leaves no step had trodden black.
+  Oh, I kept the first for another day!
+  Yet knowing how way leads on to way,
+  I doubted if I should ever come back.
+  
+  I shall be telling this with a sigh
+  Somewhere ages and ages hence:
+  Two roads diverged in a wood, and I—
+  I took the one less traveled by,
+  And that has made all the difference.`
+  
+  
+      // sign
+      const sig = await signer.signMessage(message)
+      console.log('signature:', sig)
+  
+      addNewConsoleLine(`signature: ${sig}`)
+  
+      const isValid = await sequence.utils.isValidMessageSignature(await signer.getAddress(), message, sig, provider as ethers.providers.Web3Provider)
+      console.log('isValid?', isValid)
+  
+      appendConsoleLine(`isValid? ${isValid}`)
+      setConsoleLoading(false) 
+    } catch(e) {
+      console.error(e)
+      consoleErrorMesssage()
+    }
   }
 
   const sendETH = async () => {
-    console.log(`Transfer txn on ${signer.getChainId()}`)
-
-    const toAddress = ethers.Wallet.createRandom().address
-
-    const tx1 = {
-      gasLimit: '0x55555',
-      to: toAddress,
-      value: ethers.utils.parseEther('1.234'),
-      data: '0x'
+    try {
+      resetConsole()
+  
+      console.log(`Transfer txn on ${signer.getChainId()}`)
+      addNewConsoleLine(`Transfer txn on ${signer.getChainId()}`)
+  
+      const toAddress = ethers.Wallet.createRandom().address
+  
+      const tx1 = {
+        gasLimit: '0x55555',
+        to: toAddress,
+        value: ethers.utils.parseEther('1.234'),
+        data: '0x'
+      }
+  
+      const balance1 = await provider!.getBalance(toAddress)
+      console.log(`balance of ${toAddress}, before:`, balance1)
+      appendConsoleLine(`balance of ${toAddress}, before: ${balance1}`)
+      const txnResp = await signer.sendTransaction(tx1)
+      await txnResp.wait()
+  
+      const balance2 = await provider!.getBalance(toAddress)
+      console.log(`balance of ${toAddress}, after:`, balance2)
+      appendConsoleLine(`balance of ${toAddress}, after: ${balance2}`)
+      setConsoleLoading(false) 
+    } catch(e) {
+      console.error(e)
+      consoleErrorMesssage()
     }
-
-    console.log(`balance of ${toAddress}, before:`, await provider!.getBalance(toAddress))
-
-    const txnResp = await signer.sendTransaction(tx1)
-    await txnResp.wait()
-
-    console.log(`balance of ${toAddress}, after:`, await provider!.getBalance(toAddress))
   }
 
   const sendDAI = async () => {
-    const toAddress = ethers.Wallet.createRandom().address
-
-    const amount = ethers.utils.parseUnits('5', 18)
-
-    const daiContractAddress = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063' // (DAI address on Polygon)
-
-    const tx = {
-      gasLimit: '0x55555',
-      to: daiContractAddress,
-      value: 0,
-      data: new ethers.utils.Interface(ERC_20_ABI).encodeFunctionData('transfer', [toAddress, amount.toHexString()])
+    try {
+      resetConsole()
+      const toAddress = ethers.Wallet.createRandom().address
+  
+      const amount = ethers.utils.parseUnits('5', 18)
+  
+      const daiContractAddress = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063' // (DAI address on Polygon)
+  
+      const tx = {
+        gasLimit: '0x55555',
+        to: daiContractAddress,
+        value: 0,
+        data: new ethers.utils.Interface(ERC_20_ABI).encodeFunctionData('transfer', [toAddress, amount.toHexString()])
+      }
+  
+      const txnResp = await signer.sendTransaction(tx)
+      await txnResp.wait()
+  
+      console.log('transaction response', txnResp)
+      addNewConsoleLine(`TX response ${JSON.stringify(txnResp)}`)
+      setConsoleLoading(false) 
+    } catch(e) {
+      console.error(e)
+      consoleErrorMesssage()
     }
-
-    const txnResp = await signer.sendTransaction(tx)
-    await txnResp.wait()
   }
 
   const disableActions = !isConnected
@@ -190,7 +291,7 @@ And that has made all the difference.`
               {(() => {
                 if (!connected) {
                   return (
-                    <Button onClick={openConnectModal} type="button">
+                    <Button style={{ marginBottom: '20px' }} onClick={openConnectModal} type="button">
                       Connect Wallet
                     </Button>
                   );
@@ -248,6 +349,7 @@ And that has made all the difference.`
         }}
       </ConnectButton.Custom>
       {getWalletActions()}
+      <Console message={consoleMsg} loading={consoleLoading} />
     </Container>
   )
 }

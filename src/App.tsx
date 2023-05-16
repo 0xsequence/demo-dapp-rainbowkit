806cd8e81da506e07f9ae8e0a8fe92e9b7d232cc
SamueleA <batonenbois@gmail.com>
2023-05-16 18:05:57 -0400

    updated deps

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,10 @@ const App = () => {
         sequenceWallet({ chains, connect: { app: 'Demo app', networkId: 137 } }),
         metaMaskWallet({ chains, shimDisconnect: true }),
         rainbowWallet({ chains }),
-        walletConnectWallet({ chains }),
+        walletConnectWallet({
+          chains,
+          projectId: '244ab38f5571af5263358fc67131697d',
+        }),
         injectedWallet({ chains, shimDisconnect: true })
       ]
     }

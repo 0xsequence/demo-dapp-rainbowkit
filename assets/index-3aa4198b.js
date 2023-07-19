679b19184ac9a920550e158f7ebdf81197cd13ba
Agusx1211 <agusxrun@gmail.com>
2023-07-19 15:48:40 +0000

    Build: (d5f2c50) Update rainbowkit plugin and add custom url

--- assets/index-3aa4198b.js
+++ assets/index-3aa4198b.js
@@ -1,4 +1,4 @@
-import{X as ve,Q as qe,N as k,G as W,i as u,Y as Q,$ as jr,c as U,C as ne}from"./index-0d2d243a.js";import{b as Nr}from"./browser-c5e11bc8.js";import"./index-9b17be44.js";/**
+import{X as ve,Q as qe,N as k,G as W,i as u,Y as Q,$ as jr,c as U,C as ne}from"./index-02a8784b.js";import{b as Nr}from"./browser-2ac19995.js";import"./index-0cc9c696.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause

edeb35be80476b1eb3a0978e456eb19addad2455
SamueleA <batonenbois@gmail.com>
2023-09-06 17:03:41 +0000

    Build: (3203b31) update rainbowkit plugin version

--- assets/index-c2df09e7.js
+++ assets/index-c2df09e7.js
@@ -1,4 +1,4 @@
-import{n as ve,s as qe,T as k,t as W,a as u,o as Q,R as jr,p as U,y as ne}from"./index-c1d6decb.js";import{b as Nr}from"./browser-dac94f87.js";import"./index-f622258f.js";/**
+import{n as ve,s as qe,T as k,t as W,a as u,o as Q,R as jr,p as U,y as ne}from"./index-c7f07818.js";import{b as Nr}from"./browser-fa4c7131.js";import"./index-6580baf6.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause

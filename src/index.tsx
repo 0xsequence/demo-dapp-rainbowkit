a58e027b0c83d84723a60ee1531c7b0a12e5e57d
Corban Riley <corbanbrook@gmail.com>
2024-04-04 14:00:17 -0400

    Removing web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from '@0xsequence/design-system'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 import '@0xsequence/design-system/styles.css'
 
@@ -14,8 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
